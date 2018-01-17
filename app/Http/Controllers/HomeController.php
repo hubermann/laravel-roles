aedb5d0b79d755644ccfd570818b8636e0ad0536
Hubermann <hubermann@gmail.com>
2018-01-17 14:32:16 -0300

    requiere-todo-pago

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,6 +13,7 @@ use App\ImagesProduct;
 use Cart;
 use Auth;
 use MP;
+use \Todopago;
 
 class HomeController extends Controller
 {
@@ -254,122 +255,12 @@ class HomeController extends Controller
 
             $order->save();
 
-        //común a todas los métodos
-        $http_header = array(
-            'Authorization'=>'TODOPAGO 36f1447d7fc64213b5f3fa6411cf0b07',
-            'user_agent' => 'PHPSoapClient'
-            );
-         
-        //creo instancia de la clase TodoPago
-        $connector = new Sdk($http_header, "test");
-        $operationid = $order->id ; //rand(1,10000000); 
-         
-        //opciones para el método sendAuthorizeRequest
-        $optionsSAR_comercio = array (
-            'Security'=>'36f1447d7fc64213b5f3fa6411cf0b07',
-            'EncodingMethod'=>'XML',
-            'Merchant'=>55492,
-            'URL_OK'=>"http://localhost:8000/payment_success/$operationid",
-            'URL_ERROR'=>"http://localhost:8000/payment_error/$operationid"
-        );
+        
+        //Aca hay qeu integrar TODOPAGO
 
-        $optionsSAR_operacion = array (
-            'MERCHANT'=> "2658",
-            'OPERATIONID'=>"50",
-            'CURRENCYCODE'=> 032,
-            'AMOUNT'=>"54",
+        //enviar el id de orden y luego actuualizarla dependiendo del resultado.
 
-            //Datos ejemplos CS
-            'CSBTCITY'=> "Villa General Belgrano",
-            'CSSTCITY'=> "Villa General Belgrano",
-            
-            'CSBTCOUNTRY'=> "AR",
-            'CSSTCOUNTRY'=> "AR",
-            
-            'CSBTEMAIL'=> "todopago@hotmail.com",
-            'CSSTEMAIL'=> "todopago@hotmail.com",
-            
-            'CSBTFIRSTNAME'=> "Juan",
-            'CSSTFIRSTNAME'=> "LAURA",      
-            
-            'CSBTLASTNAME'=> "Perez",
-            'CSSTLASTNAME'=> "GONZALEZ",
-            
-            'CSBTPHONENUMBER'=> "541160913988",     
-            'CSSTPHONENUMBER'=> "541160913988",     
-            
-            'CSBTPOSTALCODE'=> " 1010",
-            'CSSTPOSTALCODE'=> " 1010",
-            
-            'CSBTSTATE'=> "B",
-            'CSSTSTATE'=> "B",
-            
-            'CSBTSTREET1'=> "Cerrito 740",
-            'CSSTSTREET1'=> "Cerrito 740",
-            
-            'CSBTCUSTOMERID'=> "453458",
-            'CSBTIPADDRESS'=> "192.0.0.4",       
-            'CSPTCURRENCY'=> "ARS",
-            'CSPTGRANDTOTALAMOUNT'=> "125.38",
-            'CSMDD7'=> "",     
-            'CSMDD8'=> "Y",       
-            'CSMDD9'=> "",       
-            'CSMDD10'=> "",      
-            'CSMDD11'=> "",
-            'CSMDD12'=> "",     
-            'CSMDD13'=> "",
-            'CSMDD14'=> "",
-            'CSMDD15'=> "",        
-            'CSMDD16'=> "",
-            'CSITPRODUCTCODE'=> "electronic_good#chocho",
-            'CSITPRODUCTDESCRIPTION'=> "NOTEBOOK L845 SP4304LA DF TOSHIBA#chocho",     
-            'CSITPRODUCTNAME'=> "NOTEBOOK L845 SP4304LA DF TOSHIBA#chocho",  
-            'CSITPRODUCTSKU'=> "LEVJNSL36GN#chocho",
-            'CSITTOTALAMOUNT'=> "1254.40#10.00",
-            'CSITQUANTITY'=> "1#1",
-            'CSITUNITPRICE'=> "1254.40#15.00",
-            );
-
-
-
-            
-        //opciones para el método getAuthorizeAnswer
-        // $optionsGAA = array(    
-        //     'Security' => '8A891C0676A25FBF052D1C2FFBC82DEE', 
-        //     'Merchant' => "41702",
-        //     'RequestKey' => '83765ffb-39c8-2cce-b0bf-a9b50f405ee3',
-        //     'AnswerKey' => '9c2ddf78-1088-b3ac-ae5a-ddd45976f77d'
-        //     );
-            
-        //opciones para el método getAllPaymentMethods
-        // $optionsGAMP = array("MERCHANT"=>35);
-            
-        // //opciones para el método getStatus 
-        // $optionsGS = array('MERCHANT'=>'35', 'OPERATIONID'=>'02');
-        // $date1 = date("Y-m-d", time()-60*60*24*30);
-        // $date2 = date("Y-m-d", time());
-        // $optionsRDT = array('MERCHANT'=>2658, "STARTDATE" => $date1, "ENDDATE" => $date2, "PAGENUMBER" => 1);
-        // $devol = array(
-        //     "Security" => "108fc2b7c8a640f2bdd3ed505817ffde",
-        //     "Merchant" => "2669",
-        //     "RequestKey" => "0d801e1c-e6b1-672c-b717-5ddbe5ab97d6",
-        //     "AMOUNT" => 1.00
-        // );
-        // $anul = array(
-        //     "Security" => "108fc2b7c8a640f2bdd3ed505817ffde",
-        //     "Merchant" => "2669",
-        //     "RequestKey" => "0d801e1c-e6b1-672c-b717-5ddbe5ab97d6",
-        // );
-
-        //ejecuto los métodos
-        $rta = $connector->sendAuthorizeRequest($optionsSAR_comercio, $optionsSAR_operacion);
-        // $rta2 = $connector->getAuthorizeAnswer($optionsGAA);
-        // $rta3 = $connector->getStatus($optionsGS);
-        // $rta4 = $connector->getAllPaymentMethods($optionsGAMP);
-        // $rta5 = $connector->getByRangeDateTime($optionsRDT);
-        // $rta6 = $connector->returnRequest($devol);
-        // $rta7 = $connector->voidRequest($anul);
-
+        //El user debe poder ver sus ordenes y si es el estado es sin-pagar debe tener la posibilidad de pagar
 
     }
 
@@ -385,96 +276,6 @@ class HomeController extends Controller
     }
 
 
-    public function checkout__()
-    {
-        if( Auth::user() ){ echo "CheckOut"; }else{ return redirect('login')->with('warning', 'Please login.');}
-        if( Cart::total() == 0.00 ){ return redirect('/')->with('warning', 'No hay productos en su orden.');}
-
-
-        $items = [];
-        foreach(Cart::content() as $row)
-        {
-
-            array_push($items, ["id" => $row->id, "title" => $row->name, "quantity" => $row->qty, "currency_id" => "ARS", "unit_price" => $row->price ]);
-        }
-        //  $preference_data = array (
-        //     "items" => array (
-        //         array (
-        //             "title" => "Test2",
-        //             "quantity" => 1,
-        //             "currency_id" => "BRL",
-        //             "unit_price" => 10.41
-        //         )
-        //     )
-        // );
-
-        $preference_data = [
-            "items" => $items, 
-            "payer" => [ 
-                "name" => Auth::user()->name,
-                "surname" => "",
-                "email" => Auth::user()->email
-            ],
-            "back_urls" => [],
-            ];
-
-
-         dd($preference_data);
-
-        // try {
-        //     $preference = MP::create_preference($preference_data);
-        //     return redirect()->to($preference['response']['init_point']);
-        // } catch (Exception $e){
-        //     dd($e->getMessage());
-        // }
-
-
-
-//https://www.mercadopago.com.ar/developers/es/solutions/payments/basic-checkout/receive-payments/additional-info/
-
-
-
-// BIEN armado
-// https://github.com/mercadopago/sdk-php/blob/master/examples/checkout-buttons/basic-preference/button_full.php
-
-// lo de la vista es para hacer el boton de checkout.
-
-
-
-
-// Aplicación: 1463908 - MercadoPago application (mp-app-1463908)   Renovar credenciales
-// Checkout personalizado
-
-// Checkout básico
-
-// Modo Sandbox
-
-// Public key: TEST-80707703-e072-459f-b5a2-992726548ee2
-// Access token: TEST-1523568522842496-010920-d89e0f19b091bf4fac843dee32cbbb07__LB_LC__-1463908
-// Modo Producción
-
-// Tus credenciales de producción aún no están habilitadas, homologate primero para poder usarlas.
-// ¿Qué necesito para ir a producción? Quiero ir a producción
-// Public key: APP_USR-bf77922b-6230-4af7-9659-343a2d04b81a
-// Access token: APP_USR-1523568522842496-010920-7701973615ade0071f31a932623d799d__LA_LC__-1463908
-
-
-
-// checkout basico:
-// CLIENT_ID: 1523568522842496
-// CLIENT_SECRET: rPxUtAvrc2CDh8a1FXvgEwSoPOwpPxg2
-
-
-
-
-        //$data['preference'] = MP::create_preference($preference_data);
-
-        //return view('frontend_common.checkout_view', $data );
-#$mp = new MP('1523568522842496', 'rPxUtAvrc2CDh8a1FXvgEwSoPOwpPxg2');
-
-
-
-
-    }
+    
     
 }
